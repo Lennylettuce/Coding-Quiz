f673bf2add5b0c2abd791a942259e8c918d9f1ea
Aubrey Gunn <agunn369@gmail.com>
2023-02-26 12:48:20 -0600

    added var with questions/answers, loop for test, work on css next

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ This project was done to create a timed coding quiz using API methods to make th
 
 ## Cited Sources
 1.https://git.bootcampcontent.com/Washington-University-in-St.-Louis/WUSTL-VIRT-FSF-PT-01-2023-U-LOLC - all aspects
-2.
-3.
+2.https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS - for questions
+3.https://www.youtube.com/watch?v=LQGTb112N_c - made sense of the code for the question/answer portion of this assignment
+4.
+5.
+6.
--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,39 @@
 //code for Web API Quiz
+
+//make variable to store array of questions/answers so i can refer back to them later
+//make variable to store the user's scores
+
+var prompts = [
+{
+    question: "Commonly used data types DO NOT include: \n(a) string \n(b) boolean \n(c) alerts \n(d) numbers",
+    answer: "c"
+},
+{
+    question: "String values are enclosed in: \n(a) parenthesis \n(b) square brackets \n(c) curly brackets \n(d) <>",
+    answer: "c"
+},
+{
+    question: "Which HTML elemt does javascript go inside? \n(a) <javascript> \n(b) <script> \n(c) <scriptjs> \n(d) <js>",
+    answer: "b"
+},
+{
+    qusstion: "How do you call a function called 'myFunction' in javascript? \n(a) call myFunction() \n(b) call function myFunction() \n(c) myFunction[] \n(d) myFunction()",
+    answer: "d"
+}
+];
+
+//get prompts to show up inside the test-box area, style this in css and get loop to work
+
+var scores = 0;
+
+var test = document.querySelectorAll(".test-box");
+
+for(var i=0; i < prompts.length; i++){
+    var userAnswer = test
+    if(userAnswer == prompts[i].answer){
+        scores++;
+        alert("Correct");
+    } else{
+        alert("Wrong");
+    }
+};
